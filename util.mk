5e42da4b08c61e72ff2cb70441144202ed360cf5
jizhengkang <jzk0531@gmail.com>
2014-04-25 23:47:52 +0800

    Update util.mk

--- util.mk
+++ util.mk
@@ -1,8 +1,14 @@
 
-
 .PHONY: update
 update:
 	@echo "Update new code"
+	#firstpatch : getsmali resource
+	ifneq ($(ORGIN_SECOND_FRAMEWORK_NAME), )
+		${PORT_TOOLS}/copy_fold.sh smali/${ORGIN_SECOND_FRAMEWORK_NAME}.out/ smali/framework.jar.out/
+		rm -rf smali/${ORGIN_SECOND_FRAMEWORK_NAME}.out
+	endif
+
+
 	cat ${PORT_ROOT}/smali/sourcechange.txt ${PORT_ROOT}/last_smali/sourcechange.txt | sort | uniq > ${PORT_ROOT}/device/sourcechange.txt
 	${PORT_TOOLS}/patch_color_framework.sh ${PORT_ROOT}/last_smali/color ${PORT_ROOT}/smali/color ${PWD}/smali/ ${PORT_ROOT}/device/sourcechange.txt
 
